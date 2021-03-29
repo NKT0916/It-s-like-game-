27a33ecd83827c70aa71c2d183322b000e235300
NKT0916 <80941288+NKT0916@users.noreply.github.com>
2021-03-29 15:28:09 +0900

    Update function_other.cpp

--- function_other.cpp
+++ function_other.cpp
@@ -5,6 +5,23 @@ void Clear_Consol()
 {
 	COORD pos = { 0, 0 };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}#include <stdio.h>
+#include <Windows.h>
+
+void Clear_Consol()
+{
+	COORD pos = { 0, 0 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void HideTypingCusor(bool show)
+{
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	ConsoleCursor.bVisible = show;
+	ConsoleCursor.dwSize = 1;
+	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
 }
 
 void HideTypingCusor(bool show)
